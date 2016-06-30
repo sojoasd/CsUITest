1faa5d8ffaf74d9e2fae728f12ac32d384f25d5d
zeal <sojoasd@gmail.com>
2016-06-30 16:51:51 +0800

    create CodeUITest project

---
+++ CodeUITest/CodedUITest1.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Windows.Input;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+
+namespace CodeUITest
+{
+    /// <summary>
+    /// CodedUITest1 的摘要描述
+    /// </summary>
+    [CodedUITest]
+    public class CodedUITest1
+    {
+        public CodedUITest1()
+        {
+        }
+
+        [TestMethod]
+        public void CodedUITestMethod1()
+        {
+            // 若要為這個測試產生程式碼，請在捷徑功能表上選取 [產生自動程式碼 UI 測試的程式碼]，並選取其中一個功能表項目。
+            this.UIMap.RecordedMethod1();
+        }
+
+        #region 其他測試屬性
+
+        // 您可以使用下列其他屬性撰寫測試:
+
+        ////在每項測試執行前先使用 TestInitialize 執行程式碼
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //    // 若要為這個測試產生程式碼，請在捷徑功能表上選取 [產生自動程式碼 UI 測試的程式碼]，並選取其中一個功能表項目。
+        //}
+
+        ////在每項測試執行後使用 TestCleanup 執行程式碼
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //    // 若要為這個測試產生程式碼，請在捷徑功能表上選取 [產生自動程式碼 UI 測試的程式碼]，並選取其中一個功能表項目。
+        //}
+
+        #endregion 其他測試屬性
+
+        /// <summary>
+        ///取得或設定提供目前測試回合
+        ///相關資訊與功能的測試內容。
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        private TestContext testContextInstance;
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if ((this.map == null))
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        private UIMap map;
+    }
+}
